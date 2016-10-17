ce66239530d2c7bf1ca1403b28aa135d3071b3b4
Odissea <theo.dupuis@gmail.com>
2016-10-17 10:19:17 +0200

    Cleaning

--- generatescripts.py
+++ generatescripts.py
@@ -1,9 +1,22 @@
 import subprocess
 import os
+from usefulfunctions import *
+
+if not os.path.isdir("./LOGS") :
+	os.mkdir("./LOGS")
+	RemainingChrs = [i+ 1 for i in range(22)]
+else :
+	ProcessedChrs = list_elements("./LOGS/", extension=".log")
+	for i in range(len(ProcessedChrs)) :
+		ProcessedChrs[i] = ProcessedChrs[i].split("/")[-1]
+	RemainingChrs  = [i if i not in ProcessedChrs else None for i in range(1,23)]
+
+print(ProcessedChrs)
+print(RemainingChrs)
 
 if not os.path.isdir("Versions") :
 	os.mkdir("Versions")
-
+	
 f = open("changetofloat.py", "r")
 temp = f.read()
 f.close()
