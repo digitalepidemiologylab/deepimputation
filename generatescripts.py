d706d47f5b50b9a5660462b453c644772bb972cb
Odissea <theo.dupuis@gmail.com>
2016-10-17 10:40:03 +0200

    Cleaning

--- generatescripts.py
+++ generatescripts.py
@@ -1,27 +1,44 @@
 import subprocess
 import os
 from usefulfunctions import *
+import random
 
+numberofjobs = 4
+tobeprocessed = []
+
+
+####Clean
+if not os.path.isdir("Versions") :
+	os.mkdir("Versions")
+else :
+	subprocess.call("rm ./Versions/*.py", shell = True)
+
+#### Verify which chroms where not already processed and find a subset to work with
 if not os.path.isdir("./LOGS") :
 	os.mkdir("./LOGS")
 	RemainingChrs = [i+ 1 for i in range(22)]
 else :
 	ProcessedChrs = list_elements("./LOGS/", extension=".log")
 	for i in range(len(ProcessedChrs)) :
-		ProcessedChrs[i] = ProcessedChrs[i].split("/")[-1]
-	RemainingChrs  = [i if i not in ProcessedChrs else None for i in range(1,23)]
+		ProcessedChrs[i] = int(ProcessedChrs[i].replace("./LOGS/log", "").replace(".log", ""))
+	RemainingChrs  = [i for i in range(1,23) if i not in ProcessedChrs ]
 
-print(ProcessedChrs)
-print(RemainingChrs)
 
-if not os.path.isdir("Versions") :
-	os.mkdir("Versions")
+
+for i in range(min(numberofjobs, len(RemainingChrs))) :
+	
+	pick = random.choice(RemainingChrs)
+	if pick in tobeprocessed :
+		i -= 1
+	else :
+		tobeprocessed.append(pick)
+
 	
 f = open("changetofloat.py", "r")
 temp = f.read()
 f.close()
 
-for chrom in range(10, 19) :
+for chrom in tobeprocessed :
 	f = open("./Versions/new-job-"+str(chrom+1)+".py", "w")
 	f.write(temp.replace("%%%%%SELECTYOURFAVORITECHROMOSOME%%%%%", str(chrom+1)))
 	f.close()
