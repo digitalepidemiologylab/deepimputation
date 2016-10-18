d97ea2ae5485c97c57aa8190fc837a6050fbdbcc
Odissea <theo.dupuis@gmail.com>
2016-10-18 10:02:35 +0200

    Cleaning

--- showprog.sh
+++ showprog.sh
@@ -1,6 +1,20 @@
 #! /usr/bin/sh
 
-for dirs in /mount/SDF/1000genomeprocesseddata/floatfiles/*; do
-echo $dirs
-ls $dirs | wc -l | awk '{print $1*100/2504"%"}'
+max=100
+min=0
+
+Proggress_bar(){
+	echo $1
+	
+}
+
+#PATH="/mount/SDF/1000genomeprocesseddata/floatfiles/*"
+_PATH="./floatfiles/*"
+
+for dirs in $_PATH; do
+chrom=$(basename $dirs)
+echo $chrom
+percdone=$(ls $dirs | wc -l | awk '{print $1*100/2504"%"}')
+Proggress_bar $percdone
+
 done
